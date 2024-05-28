49435e381750105d39846e8c435ec8d5914c381f
Montrell Alen <montrell.allen@atlasschol.com>
2024-05-28 03:49:17 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies int a and int b
+ * @a: first integer being multiplied
+ * @b: second integer being mulitplied
+ * Return: result of a * b
+*/
+
+int mul(int a, int b)
+
+{
+	return ((a * b));
+}
