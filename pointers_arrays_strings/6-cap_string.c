101bf21b9313575b94330e5d3b8ab185aa723345
Montrell Alen <montrell.allen@atlasschol.com>
2024-06-03 20:27:00 +0000

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * cap_string - capatlizing all first letters
+ * @str: string pointer
+ * Return: Always 0
+*/
+
+char *cap_string(char *str)
+{
+	int a:
+
+	for (a = 0 ; str[a] != '\0'; a++)
+	{
