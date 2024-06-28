3a64a2e52b7fb809d35e4fac712a3b904c325892
Montrell Alen <montrell.allen@atlasschol.com>
2024-06-28 17:32:38 +0000

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,10 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - creates a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ * Return: NULL if function fails
+*/
