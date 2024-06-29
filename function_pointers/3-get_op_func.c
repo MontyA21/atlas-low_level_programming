fe949c38defecf51630481aa8f252a2d27f73312
Montrell Alen <montrell.allen@atlasschol.com>
2024-06-29 04:33:18 +0000

    3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,23 +1,28 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <stdio.h>
-int op_add(int a, int b)
+#include <string.h>
+/**
+ * get_op_func - selects correct function
+ * @s: operator passed as arg
+ * Return: a pointer to the function that corresponds to the operator given as a parameter.
+*/
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-int op_mod(int a, int b)
-{
-	return (a % b);
+op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+	while (i < 5)
+	{
+		if(strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (0);
 }
