85697a7ecad15799d063e01ccbb7b2baeb8029ca
Montrell Alen <montrell.allen@atlasschol.com>
2024-06-29 04:27:37 +0000

    3-main.c

---
+++ function_pointers/3-main.c
@@ -0,0 +1,9 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the result of operation
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ * Return: Always 0
+*/
