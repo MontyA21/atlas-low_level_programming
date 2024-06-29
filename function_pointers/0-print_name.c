296a5550235e0899f818a0538fb6e2cb0f64f84c
Montrell Alen <montrell.allen@atlasschol.com>
2024-06-29 04:13:19 +0000

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function thats prints name
+ * Return: Void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	if (f == NULL)
+		return;
+
+	f(name);
